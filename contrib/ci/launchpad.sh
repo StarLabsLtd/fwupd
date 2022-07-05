e29041481e617331dd7b82890969ae8ba84523e3
Sean Rhodes <sean@starlabs.systems>
2022-07-05 08:44:45 +0100

    contrib: Add build files for launchpad
    Signed-off-by: Sean Rhodes <sean@starlabs.systems>

---
+++ contrib/ci/launchpad.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+set -e
+set -x
+
+#check for and install missing dependencies
+./contrib/ci/fwupd_setup_helpers.py install-dependencies --yes -o ubuntu
+
+#check we have pip
+./contrib/ci/fwupd_setup_helpers.py install-pip --yes -o ubuntu
+
+#check meson is new enough
+./contrib/ci/fwupd_setup_helpers.py test-meson
+
+#check markdown is new enough
+./contrib/ci/fwupd_setup_helpers.py test-markdown
+
+#clone test firmware if necessary
+. ./contrib/ci/get_test_firmware.sh
+
+#evaluate using Ubuntu's buildflags
+#evaluate using Debian/Ubuntu's buildflags
+#disable link time optimization, Ubuntu currently only sets it for GCC
+export DEB_BUILD_MAINT_OPTIONS="optimize=-lto"
+eval "$(dpkg-buildflags --export=sh)"
+#filter out -Bsymbolic-functions
+LDFLAGS=$(dpkg-buildflags --get LDFLAGS | sed "s/-Wl,-Bsymbolic-functions\s//")
+export LDFLAGS
+
+root=$(pwd)
+rm -rf ${root}/build
+chown -R nobody ${root}
+sudo -u nobody meson ${root}/build -Dman=false -Ddocs=enabled -Dgusb:tests=false -Dplugin_platform_integrity=true --prefix=${root}/dist
+#build with clang
+sudo -u nobody ninja -C ${root}/build test -v
+
+#make docs available outside of docker
+ninja -C ${root}/build install -v
+#prepare
+export DEBFULLNAME="Sean Rhodes"
+export DEBEMAIL="sean@starlabs.systems"
+
+VERSION=`head meson.build | grep ' version:' | cut -d \' -f2`
+
+#build the package
+EDITOR=/bin/true dch --create --package fwupd --distribution focal -v $VERSION "CI Build"
+
+debuild -S
